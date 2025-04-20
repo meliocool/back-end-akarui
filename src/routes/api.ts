8b0155ab40f65c5f64a5b3e830d4337ce4ccd63d
melio <dhitanimam05@gmail.com>
2025-04-20 11:11:50 +0700

    feat: CRUD API Ticket

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -8,6 +8,7 @@ import mediaController from "../controllers/media.controller";
 import categoryController from "../controllers/category.controller";
 import regionController from "../controllers/region.controller";
 import eventController from "../controllers/event.controller";
+import ticketController from "../controllers/ticket.controller";
 
 const router = express.Router();
 
@@ -160,6 +161,26 @@ router.get(
   #swagger.tags = ['Events']
   */
 );
+
+// -- TICKETS -- //
+router.post(
+  "/tickets",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN])],
+  ticketController.create
+);
+router.get("/tickets", ticketController.findAll);
+router.get("/tickets/:id", ticketController.findOne);
+router.put(
+  "/tickets/:id",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN])],
+  ticketController.update
+);
+router.delete(
+  "/tickets/:id",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN])],
+  ticketController.remove
+);
+router.get("/tickets/:eventId/events", ticketController.findAllByEvent);
 
 // -- LOCATION -- //
 router.get(
